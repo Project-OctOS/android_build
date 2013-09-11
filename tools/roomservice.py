8f8455fe5070b852729f30024a98b2509b12e6fa
treken <chupekevin@gmail.com>
2013-09-11 12:01:35 -0500

    revert commit

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -91,7 +91,7 @@ def indent(elem, level=0):
 def get_default_revision():
     m = ElementTree.parse(".repo/manifests/default.xml")
     d = m.findall('default')[0]
-    r = d.get('branch')
+    r = d.get('revision')
     return r.split('/')[-1]
 
 def get_from_manifest(devicename):
